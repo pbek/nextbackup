84235959ced6a7171e30e794114e4dc9a13eab9b
Patrizio Bekerle <office@bekerle.com>
2015-07-18 16:21:03 +0200

    made design changes

--- templates/part.content.php
+++ templates/part.content.php
@@ -1,8 +1,11 @@
-<form class="section">
+<div class="section">
     <h2>
-        <label for="backup-date-select"><?php p($l->t('Backup date'));?></label>
+        <label for="backup-date-select"><?php p($l->t('Your backups'));?></label>
     </h2>
-    <select class="chosen-select" id="backup-date-select" name="timestamp" data-placeholder="<?php p($l->t('Select the backup date'));?>">
+    <div class="box">
+        <?php p($l->t('Select the backup you want to restore tables from.'));?>
+    </div>
+    <select class="chosen-select" id="backup-date-select" name="timestamp" data-placeholder="<?php p($l->t('Select backup'));?>">
         <option value="0" selected=""></option>
         <?php foreach($_['backupDateHash'] as $timestamp => $date):?>
             <option value="<?php p($timestamp);?>">
@@ -10,14 +13,17 @@
             </option>
         <?php endforeach;?>
     </select>
-    <div id="backup-tables-block">
-        <h2>
-            <label for="backup-tables-select"><?php p($l->t('Backup tables'));?></label>
-        </h2>
-        <select id="backup-tables-select" multiple="" name="tables" data-placeholder="<?php p($l->t('Select the tables to restore'));?>"></select>
-        <input type="button" id="restore-button" value="<?php p($l->t('Restore tables'));?>">
+</div>
+<div class="section" id="backup-tables-block">
+    <h2>
+        <label for="backup-tables-select"><?php p($l->t('Tables of backup'));?></label>
+    </h2>
+    <div class="box">
+        <?php p($l->t('Select the tables you want to restore.'));?>
     </div>
-    <p>
-        <input type="button" id="backup-button" value="<?php p($l->t('Create Backup'));?>">
-    </p>
-</form>
+    <select id="backup-tables-select" multiple="" name="tables" data-placeholder="<?php p($l->t('Select the tables to restore'));?>"></select>
+    <input type="button" id="restore-button" value="<?php p($l->t('Restore tables'));?>">
+</div>
+<div class="section">
+    <input type="button" id="backup-button" value="<?php p($l->t('Create Backup'));?>">
+</div>
