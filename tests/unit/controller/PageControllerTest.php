1e39d110d3756fb136b217b5a1e3d77be254d5b8
Patrizio Bekerle <office@bekerle.com>
2015-07-20 07:42:27 +0200

    fixed tests and added some tests

--- tests/unit/controller/PageControllerTest.php
+++ tests/unit/controller/PageControllerTest.php
@@ -12,7 +12,7 @@
 namespace OCA\OwnBackup\Controller;
 
 use PHPUnit_Framework_TestCase;
-
+use OCP\AppFramework\Http\DataResponse;
 use OCP\AppFramework\Http\TemplateResponse;
 
 
@@ -23,18 +23,44 @@ class PageControllerTest extends PHPUnit_Framework_TestCase {
 
 	public function setUp() {
 		$request = $this->getMockBuilder('OCP\IRequest')->getMock();
+		$backupService = $this->getMockBuilder('OCA\OwnBackup\Service\BackupService')->getMock();
 
 		$this->controller = new PageController(
-			'ownbackup', $request, $this->userId
+			'ownbackup', $request, $backupService, $this->userId
 		);
 	}
-
 
 	public function testIndex() {
 		$result = $this->controller->index();
 
-		$this->assertEquals(['user' => 'john'], $result->getParams());
+		$this->assertEquals(['backupDateHash' => NULL], $result->getParams());
 		$this->assertEquals('main', $result->getTemplateName());
 		$this->assertTrue($result instanceof TemplateResponse);
+	}
+
+	public function testDoRestoreTables() {
+		$timestamp = time();
+		$tables = ["some_table"];
+
+		$result = $this->controller->doRestoreTables( $timestamp, $tables );
+
+		$this->assertEquals(['message' => '1 table(s) have been restored.'], $result->getData());
+		$this->assertTrue($result instanceof DataResponse);
+	}
+
+	public function testDoFetchTables() {
+		$timestamp = time();
+
+		$result = $this->controller->doFetchTables( $timestamp );
+
+		$this->assertEquals(['tables' => NULL], $result->getData());
+		$this->assertTrue($result instanceof DataResponse);
+	}
+
+	public function testDoCreateBackup() {
+		$result = $this->controller->doCreateBackup();
+
+		$this->assertEquals(['timestamps' => NULL], $result->getData());
+		$this->assertTrue($result instanceof DataResponse);
 	}
 }
